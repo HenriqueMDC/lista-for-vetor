a4512102e3769bb3468a627aab67941bdaa3d651
Henque Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-04-27 14:16:00 -0300

    Exercicio 01 com for e vetor
    Solicitei 25 nomes e apresentei os mesmos

---
+++ Lista-for-vetor/Lista-for-vetor/Exercicio01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lista_for_vetor
+{
+    class Exercicio01
+    {
+        public Exercicio01()
+        {
+            string[] nomes = new string[25];
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("Digite os nomes: ");
+                nomes[i] = Console.ReadLine();
+            }
+            Console.Clear();
+
+            Console.WriteLine("Pessoas cadastradas: ");
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("Nomes: " + nomes[i]);
+            }
+        }
+    }
+}
