4ca9e3286497d8b931de9252d6dda744c45edecb
Henque Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-04-27 14:59:03 -0300

    Exercicio 03 com for e vetor
    Solicitar a quantidade  de cadastro, solicitar os valores e aoresentar a media

---
+++ Lista-for-vetor/Lista-for-vetor/Exercicio03.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lista_for_vetor
+{
+    class Exercicio03
+    {
+        public Exercicio03()
+        {
+            Console.WriteLine("Digite a quantida de produtos que queres cadastrar: ");
+            int quantidadeRegistro = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+
+            double[] valores = new double[quantidadeRegistro];
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                Console.WriteLine("Valor dos produtos: ");
+                valores[i] = Convert.ToDouble(Console.ReadLine());
+
+                Console.Clear();
+            }
+
+            double somaValores = 0;
+            for (int i = 0; i < valores.Length; i++)
+            {
+                somaValores = valores[i] + somaValores;
+            }
+
+            Console.WriteLine("Valor total : " + somaValores);
+        }
+    }
+}
