011331994eb6bd683f35ff8a19e0fdad940c168d
Henque Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-04-27 16:25:38 -0300

    Exercicio 05 com for e vetor
    Solicitei a quantidades de jogos que qerias cadastra, depois foi solicitado o nome e a quantidade em estoque, e foi apresentado o mesmo

---
+++ Lista-for-vetor/Lista-for-vetor/Exercicio05.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lista_for_vetor
+{
+    class Exercicio05
+    {
+        public Exercicio05()
+        {
+            Console.WriteLine("Digite a quantidade de jogos que queres cadastrar: ");
+            int quantidadeJogos = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            int[] quantidadeEstoque = new int[quantidadeJogos];
+            string[] nomeJogo = new string[quantidadeJogos];
+
+            for (int i = 0; i < nomeJogo.Length; i++)
+            {
+                Console.WriteLine("Nome do Jogo: ");
+                nomeJogo[i] = Console.ReadLine();
+                Console.WriteLine("QUantidade do jogo: ");
+                quantidadeEstoque[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+            for (int i = 0; i < nomeJogo.Length; i++)
+            {
+                Console.WriteLine(nomeJogo[i] +  " tem " + quantidadeEstoque[i] + " guardados");
+            }
+
+
+        }
+    }
+}
