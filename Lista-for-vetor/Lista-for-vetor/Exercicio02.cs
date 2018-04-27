0855fcb7bec84c7fa332275f4064d126f499e7b3
Henque Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-04-27 14:21:30 -0300

    Exercicio 02 com for e vetor
    Solicitei 7 idade e apresentei os mesmos

---
+++ Lista-for-vetor/Lista-for-vetor/Exercicio02.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lista_for_vetor
+{
+    class Exercicio02
+    {
+        public Exercicio02()
+        {
+            int[] idades = new int[7];
+
+            for (int i = 0; i < idades.Length; i++)
+            {
+                Console.WriteLine("Digite as idades: ");
+                idades[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Clear();
+
+            Console.WriteLine("Idades cadastradas: ");
+
+            for (int i = 0; i < idades.Length; i++)
+            {
+                Console.WriteLine("Idades: " + idades[i]);
+            }
+        }
+    }
+}
